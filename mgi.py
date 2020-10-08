5511342174a3f7ecc94e45299721d1ad0fc5c573
AlexandreLanglade <developjtaa@outlook.fr>
2020-10-08 16:17:08 +0200

    np.arctan2

--- mgi.py
+++ mgi.py
@@ -1,5 +1,5 @@
 import numpy as np
-from math import atan2
+
 '''
 Mind :
 t11 = t23 = t14
@@ -20,13 +20,13 @@ def read_xyz():
 def calculus_qi(t, epsilon=1):
     result = []
     if(t[0][0] == 0 and t[1][0] == 0):
-        q1 = atan2((epsilon*t[0][3])/(np.sqrt(t[0][3]**2+t[1][3]**2)),((-1)*epsilon*t[1][3])/(np.sqrt(t[0][3]**2+t[1][3]**2)))
+        q1 = np.arctan2((epsilon*t[0][3])/(np.sqrt(t[0][3]**2+t[1][3]**2)),((-1)*epsilon*t[1][3])/(np.sqrt(t[0][3]**2+t[1][3]**2)))
     else :     
-        q1 = atan2((epsilon*t[0][0])/(np.sqrt(t[0][0]**2+t[1][0]**2)),((-1)*epsilon*t[1][1])/(np.sqrt(t[0][0]**2+t[1][0]**2)))
+        q1 = np.arctan2((epsilon*t[0][0])/(np.sqrt(t[0][0]**2+t[1][0]**2)),((-1)*epsilon*t[1][1])/(np.sqrt(t[0][0]**2+t[1][0]**2)))
     result.append(q1)
     q2 = (-1) * t[0][3] * np.sin(q1) + t[1][3] * np.cos(q1)
     result.append(q2)
-    q3 = atan2(t[0][0]*np.sin(q1)-t[1][0]*np.cos(q1),t[2][1])
+    q3 = np.arctan2(t[0][0]*np.sin(q1)-t[1][0]*np.cos(q1),t[2][1])
     result.append(q3)
     return result
 
